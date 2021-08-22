d2b6696e393764de877a5f770ff20ece1d19cf3d
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-08-22 01:43:06 +0000

    admin profile and image update part 3

--- yeyam_shop/resources/views/admin/admin_profile_edit.blade.php
+++ yeyam_shop/resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <div class="container-full">
         <div class="box">
             <div class="box-header with-border">
@@ -36,11 +37,11 @@
                                         <div class="form-group">
                                             <h5>File Input Field <span class="text-danger">*</span></h5>
                                             <div class="controls">
-                                                <input type="file" name="profile_photo_path" class="form-control" required=""> <div class="help-block"></div></div>
+                                                <input type="file" name="profile_photo_path" class="form-control" required="" id="image"> </div>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
-                                        <img src="{{ (!empty($adminData->profile_photo_path)) ?
+                                        <img id="showImage" src="{{ (!empty($adminData->profile_photo_path)) ?
                     url('upload/admin_images/'.$adminData->profile_photo_path) : url('upload/no_image.jpg') }}" style="width: 100px; height: 100px;" alt="">
                                     </div>
                                 </div>
@@ -59,4 +60,16 @@
             <!-- /.box-body -->
         </div>
     </div>
+
+    <script type="text/javascript">
+        $(document).ready(function (){
+            $('#image').change(function (e){
+                var reader=new FileReader();
+                reader.onload=function (e){
+                    $('#showImage').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(e.target.files['0']);
+            })
+        });
+    </script>
 @endsection
