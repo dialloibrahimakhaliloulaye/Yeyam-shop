c6bd197a095c0fc54e46795e5e74517abf8b1252
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-08-24 18:37:13 +0000

    user profile design part 3

--- yeyam_shop/app/Http/Controllers/Frontend/IndexController.php
+++ yeyam_shop/app/Http/Controllers/Frontend/IndexController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-use http\Client\Curl\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
@@ -27,5 +27,28 @@ class IndexController extends Controller
         $id=Auth::user()->id;
         $user=User::find($id);
         return view('frontend.profile.user_profile', compact('user'));
+    }
+
+    public function UserProfileStore(Request $request)
+    {
+        $data=User::find(Auth::user()->id);
+        $data->name=$request->name;
+        $data->email=$request->email;
+        $data->email=$request->phone;
+
+        if ($request->file('profile_photo_path')){
+            $file=$request->file('profile_photo_path');
+            @unlink(public_path('upload/user_images/'.$data->profile_photo_path));
+            $filename=date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/user_images'), $filename);
+            $data['profile_photo_path']=$filename;
+        }
+        $data->save();
+
+        $notification=array(
+            'message'=>'User Profile updated successfully',
+            'alert-type'=>'success'
+        );
+        return redirect()->route('dashboard')->with($notification);
     }
 }
