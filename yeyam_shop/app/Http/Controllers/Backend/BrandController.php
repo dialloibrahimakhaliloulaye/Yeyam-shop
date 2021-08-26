6c6c69b3c73cc32ba136d8707f71c09fe8b8eba1
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-08-26 16:13:08 +0000

    brand crud part 1

--- yeyam_shop/app/Http/Controllers/Backend/BrandController.php
+++ yeyam_shop/app/Http/Controllers/Backend/BrandController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Brand;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use function Livewire\str;
 
 class BrandController extends Controller
 {
@@ -16,6 +18,24 @@ class BrandController extends Controller
 
     public function BrandStore(Request $request)
     {
+        $request->validate([
+            'brand_name'=>'required',
+            'brand_image'=>'required|mimes:jpg, jpeg, png, svg'
+        ]);
+        $images=$request->file('brand_image');
+        $name_gen=hexdec(uniqid()).'.'.$images->getClientOriginalExtension();
+        Image::make($images)->resize(300,300)->save('upload/brand/'.$name_gen);
+        $save_url='upload/brand/'.$name_gen;
 
+        Brand::insert([
+            'brand_name'=>$request->brand_name,
+            'brand_slug'=>strtolower(str_replace(' ', '-', $request->brand_name)),
+            'brand_image'=>$save_url,
+        ]);
+        $notification=array(
+            'message'=>'Brand created successfully',
+            'alert-type'=>'success'
+        );
+        return redirect()->back()->with($notification);
     }
 }
--- yeyam_shop/resources/views/backend/brand/brand_view.blade.php
+++ yeyam_shop/resources/views/backend/brand/brand_view.blade.php
@@ -60,12 +60,18 @@
                                             <h5>Brand name<span class="text-danger">*</span></h5>
                                             <div class="controls">
                                                 <input type="text" name="brand_name" class="form-control" > </div>
+                                            @error('brand_name')
+                                            <span class="text-danger">{{$message}}</span>
+                                            @enderror
                                         </div>
 
                                         <div class="form-group">
                                             <h5>Brand image<span class="text-danger">*</span></h5>
                                             <div class="controls">
                                                 <input type="file" name="brand_image" class="form-control" > </div>
+                                            @error('brand_image')
+                                            <span class="text-danger">{{$message}}</span>
+                                            @enderror
                                         </div>
                                         <div class="text-xs-right">
                                             <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Add">
