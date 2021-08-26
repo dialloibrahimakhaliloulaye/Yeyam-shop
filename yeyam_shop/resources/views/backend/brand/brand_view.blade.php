85287003007d0148debf0f0423800b45c1ea0472
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-08-26 13:14:21 +0000

    admin brand page setup part 2

--- yeyam_shop/resources/views/backend/brand/brand_view.blade.php
+++ yeyam_shop/resources/views/backend/brand/brand_view.blade.php
@@ -1,4 +1,57 @@
 @extends('admin.admin_master')
 @section('admin')
 
+    <!-- Content Wrapper. Contains page content -->
+
+        <div class="container-full">
+            <!-- Content Header (Page header) -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="row">
+                    <div class="col-12">
+
+                        <div class="box">
+                            <div class="box-header with-border">
+                                <h3 class="box-title">Brand List</h3>
+                            </div>
+                            <!-- /.box-header -->
+                            <div class="box-body">
+                                <div class="table-responsive">
+                                    <table id="example1" class="table table-bordered table-striped">
+                                        <thead>
+                                        <tr>
+                                            <th>Brand name</th>
+                                            <th>Image</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($brands as $item)
+                                        <tr>
+                                            <td>{{$item->brand_name}}</td>
+                                            <td><img src="{{asset($item->brand_image)}}" alt="" style="width: 70px; height: 40px;"></td>
+                                            <td>
+                                                <a href="" class="btn btn-info">Edit</a>
+                                                <a href="" class="btn btn-danger">Delete</a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <!-- /.box-body -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </section>
+            <!-- /.content -->
+
+        </div>
+
+    <!-- /.content-wrapper -->
 @endsection
