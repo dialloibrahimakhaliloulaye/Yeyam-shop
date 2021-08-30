096c3a9364987b8ff969cece83bb5e9d04aa513a
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-08-30 20:35:18 +0000

    slider active and inactive

--- yeyam_shop/app/Http/Controllers/Backend/SliderController.php
+++ yeyam_shop/app/Http/Controllers/Backend/SliderController.php
@@ -97,8 +97,26 @@ class SliderController extends Controller
             'message' => 'Slider Delectd Successfully',
             'alert-type' => 'info'
         );
-
         return redirect()->back()->with($notification);
+    } // end method
 
+    public function SliderInactive($id){
+        Slider::findOrFail($id)->update(['status' => 0]);
+
+        $notification = array(
+            'message' => 'Slider Inactive Successfully',
+            'alert-type' => 'info'
+        );
+        return redirect()->back()->with($notification);
+    } // end method
+
+    public function SliderActive($id){
+        Slider::findOrFail($id)->update(['status' => 1]);
+
+        $notification = array(
+            'message' => 'Slider Active Successfully',
+            'alert-type' => 'info'
+        );
+        return redirect()->back()->with($notification);
     } // end method
 }
