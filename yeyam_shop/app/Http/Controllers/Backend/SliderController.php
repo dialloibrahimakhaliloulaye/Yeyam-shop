61e0795712d525bdd53d682c870e8b83018f4e78
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-08-30 19:50:34 +0000

    upload slider and show all slider list part 2

--- yeyam_shop/app/Http/Controllers/Backend/SliderController.php
+++ yeyam_shop/app/Http/Controllers/Backend/SliderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Slider;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class SliderController extends Controller
 {
@@ -12,4 +13,32 @@ class SliderController extends Controller
         $sliders = Slider::latest()->get();
         return view('backend.slider.slider_view',compact('sliders'));
     }
+
+    public function SliderStore(Request $request){
+
+        $request->validate([
+            'slider_img' => 'required',
+        ],[
+            'slider_img.required' => 'Plz Select One Image',
+        ]);
+
+        $image = $request->file('slider_img');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(870,370)->save('upload/slider/'.$name_gen);
+        $save_url = 'upload/slider/'.$name_gen;
+
+        Slider::insert([
+            'title' => $request->title,
+            'description' => $request->description,
+            'slider_img' => $save_url,
+        ]);
+
+        $notification = array(
+            'message' => 'Slider Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+    } // end method
 }
