bdf92348a52ce513fc583fad693494c2e7547ac8
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-08-31 14:05:52 +0000

    frontend dynamic main sliders

--- yeyam_shop/app/Http/Controllers/Frontend/IndexController.php
+++ yeyam_shop/app/Http/Controllers/Frontend/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Slider;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,7 +17,8 @@ class IndexController extends Controller
     public function Index()
     {
         $categories=Category::orderBy('category_name', 'ASC')->get();
-        return view('frontend.index', compact('categories'));
+        $sliders=Slider::where('status', 1)->orderBy('id', 'DESC')->limit(3)->get();
+        return view('frontend.index', compact('categories', 'sliders'));
     }
 
     public function UserLogout()
