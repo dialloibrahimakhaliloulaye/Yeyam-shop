ea4e38f4d2c3310b6281caf5bdb700f1f66788d0
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-09-01 16:50:26 +0000

    product show with skip category & brand part 1

--- yeyam_shop/resources/views/frontend/index.blade.php
+++ yeyam_shop/resources/views/frontend/index.blade.php
@@ -662,6 +662,81 @@
                     </section>
                     <!-- /.section -->
                     <!-- ============================================== FEATURED PRODUCTS : END ============================================== -->
+                    <!-- ============================================== SKIP PRODUCT 0 ============================================== -->
+                    <section class="section featured-product wow fadeInUp">
+                        <h3 class="section-title">{{$skip_category_0->category_name}}</h3>
+                        <div class="owl-carousel home-owl-carousel custom-carousel owl-theme outer-top-xs">
+                            @foreach($skip_product_0 as $product)
+                                <div class="item item-carousel">
+                                    <div class="products">
+                                        <div class="product">
+                                            <div class="product-image">
+                                                <div class="image"> <a href="{{url('product/details/'.$product->id.'/'.$product->product_slug)}}">
+                                                        <img  src="{{asset($product->product_thambnail)}}" alt=""></a> </div>
+                                                <!-- /.image -->
+                                                @php
+                                                    $amount=$product->selling_price - $product->discount_price;
+                                                    $discount=($amount/$product->selling_price) * 100;
+                                                @endphp
+                                                <div>
+                                                    @if($product->discount_price==NULL)
+                                                        <div class="tag new"><span>new</span></div>
+                                                    @else
+                                                        <div class="tag hot"><span>{{'- '.round($discount)}}%</span></div>
+                                                    @endif
+                                                </div>
+
+                                            </div>
+                                            <!-- /.product-image -->
+
+                                            <div class="product-info text-left">
+                                                <h3 class="name"><a href="{{url('product/details/'.$product->id.'/'.$product->product_slug)}}">
+                                                        {{$product->product_name}}</a></h3>
+                                                <div class="rating rateit-small"></div>
+                                                <div class="description"></div>
+                                                @if($product->discount_price==NULL)
+                                                    <div class="product-price">
+                                                        <span class="price-before-discount">{{$product->discount_price}} FCFA</span>
+                                                    </div>
+                                                @else
+                                                    <div class="product-price">
+                                                        <span class="price"> {{$product->discount_price}} FCFA </span>
+                                                        <span class="price-before-discount">{{$product->selling_price}} FCFA</span>
+                                                    </div>
+                                            @endif
+
+                                            <!-- /.product-price -->
+
+                                            </div>
+                                            <!-- /.product-info -->
+                                            <div class="cart clearfix animate-effect">
+                                                <div class="action">
+                                                    <ul class="list-unstyled">
+                                                        <li class="add-cart-button btn-group">
+                                                            <button data-toggle="tooltip" class="btn btn-primary icon" type="button" title="Add Cart"> <i class="fa fa-shopping-cart"></i> </button>
+                                                            <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+                                                        </li>
+                                                        <li class="lnk wishlist"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
+                                                        <li class="lnk"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal" aria-hidden="true"></i> </a> </li>
+                                                    </ul>
+                                                </div>
+                                                <!-- /.action -->
+                                            </div>
+                                            <!-- /.cart -->
+                                        </div>
+                                        <!-- /.product -->
+
+                                    </div>
+                                    <!-- /.products -->
+                                </div>
+                                <!-- /.item -->
+                            @endforeach
+
+                        </div>
+                        <!-- /.home-owl-carousel -->
+                    </section>
+                    <!-- /.section -->
+                    <!-- ============================================== SKIP PRODUCT 0 : END ============================================== -->
                     <!-- ============================================== WIDE PRODUCTS ============================================== -->
                     <div class="wide-banners wow fadeInUp outer-bottom-xs">
                         <div class="row">
