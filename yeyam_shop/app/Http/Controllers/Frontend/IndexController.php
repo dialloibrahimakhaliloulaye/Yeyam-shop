79ddd3fb6a9d4c1d77cade478bbe0bde39c416d6
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-09-01 14:53:17 +0000

    hot deals product show

--- yeyam_shop/app/Http/Controllers/Frontend/IndexController.php
+++ yeyam_shop/app/Http/Controllers/Frontend/IndexController.php
@@ -22,7 +22,9 @@ class IndexController extends Controller
         $sliders=Slider::where('status', 1)->orderBy('id', 'DESC')->limit(3)->get();
         $products=Product::where('status', 1)->orderBy('id', 'DESC')->limit(6)->get();
         $featured=Product::where('featured', 1)->orderBy('id', 'DESC')->limit(6)->get();
-        return view('frontend.index', compact('categories', 'sliders', 'products', 'featured'));
+        $hot_deals=Product::where('hot_deals', 1)->orderBy('id', 'DESC')->limit(6)->get();
+        return view('frontend.index', compact('categories', 'sliders',
+            'products', 'featured', 'hot_deals'));
     }
 
     public function UserLogout()
