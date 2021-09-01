2dbcdadc3b273b4deca7a2134a281397574b60cf
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-09-01 18:16:48 +0000

    product tags show

---
+++ yeyam_shop/resources/views/frontend/common/product_tags.blade.php
@@ -0,0 +1,16 @@
+@php
+    $tags=\App\Models\Product::groupBy('product_tags')->select('product_tags')->get();
+@endphp
+
+<div class="sidebar-widget product-tag wow fadeInUp">
+    <h3 class="section-title">Product tags</h3>
+    <div class="sidebar-widget-body outer-top-xs">
+        <div class="tag-list">
+            @foreach($tags as $tag)
+            <a class="item" title="Phone" href="category.html"><strong>{{$tag->product_tags }}</strong></a>
+            @endforeach
+        </div>
+        <!-- /.tag-list -->
+    </div>
+    <!-- /.sidebar-widget-body -->
+</div>
