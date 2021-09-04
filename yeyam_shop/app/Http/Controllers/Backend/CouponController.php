e78d10fceb56799d263436929168cba6fdbacd43
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-09-04 01:01:47 +0000

    admin panel : create coupon system part 2

--- yeyam_shop/app/Http/Controllers/Backend/CouponController.php
+++ yeyam_shop/app/Http/Controllers/Backend/CouponController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Coupon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class CouponController extends Controller
 {
@@ -12,4 +13,30 @@ class CouponController extends Controller
         $coupons = Coupon::orderBy('id','DESC')->get();
         return view('backend.coupon.view_coupon',compact('coupons'));
     }
+
+    public function CouponStore(Request $request){
+
+        $request->validate([
+            'coupon_name' => 'required',
+            'coupon_discount' => 'required',
+            'coupon_validity' => 'required',
+
+        ]);
+
+        Coupon::insert([
+            'coupon_name' => strtoupper($request->coupon_name),
+            'coupon_discount' => $request->coupon_discount,
+            'coupon_validity' => $request->coupon_validity,
+            'created_at' => Carbon::now(),
+
+        ]);
+
+        $notification = array(
+            'message' => 'Coupon Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+    } // end method
 }
