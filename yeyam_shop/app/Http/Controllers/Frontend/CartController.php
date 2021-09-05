e5f01243adbf1d0bd3805836be6a0ca3a770cc27
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-09-05 18:40:23 +0000

    checkout page ¦ shpping address part 2

--- yeyam_shop/app/Http/Controllers/Frontend/CartController.php
+++ yeyam_shop/app/Http/Controllers/Frontend/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Coupon;
 use App\Models\Product;
+use App\Models\ShipDivision;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
 use App\Models\Wishlist;
@@ -142,7 +143,8 @@ class CartController extends Controller
                 $carts = Cart::content();
                 $cartQty = Cart::count();
                 $cartTotal = Cart::total();
-                return view('frontend.checkout.checkout_view',compact('carts','cartQty','cartTotal'));
+                $divisions = ShipDivision::orderBy('division_name','ASC')->get();
+                return view('frontend.checkout.checkout_view',compact('carts','cartQty','cartTotal','divisions'));
             }else{
                 $notification = array(
                     'message' => 'Shopping At list One Product',
