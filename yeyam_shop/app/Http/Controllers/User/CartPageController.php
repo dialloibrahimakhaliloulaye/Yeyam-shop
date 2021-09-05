976c2923f3fc486d1e57a183306f6beb8b100d77
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-09-05 02:29:28 +0000

    coupon remove & discount with ajax part 3

--- yeyam_shop/app/Http/Controllers/User/CartPageController.php
+++ yeyam_shop/app/Http/Controllers/User/CartPageController.php
@@ -29,6 +29,9 @@ class CartPageController extends Controller
 
     public function RemoveCartProduct($rowId){
         Cart::remove($rowId);
+        if (Session::has('coupon')) {
+            Session::forget('coupon');
+        }
         return response()->json(['success' => 'Successfully Remove From Cart']);
     }
 
