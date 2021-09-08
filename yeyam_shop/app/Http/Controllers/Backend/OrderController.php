6a95e59948632bad2868781e48f19a0be220707f
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-09-08 15:25:02 +0000

    admin panel ¦ order status update part 2

--- yeyam_shop/app/Http/Controllers/Backend/OrderController.php
+++ yeyam_shop/app/Http/Controllers/Backend/OrderController.php
@@ -26,7 +26,7 @@ class OrderController extends Controller
 
     // Confirmed Orders
     public function ConfirmedOrders(){
-        $orders = Order::where('status','confirm')->orderBy('id','DESC')->get();
+        $orders = Order::where('status','confirmed')->orderBy('id','DESC')->get();
         return view('backend.orders.confirmed_orders',compact('orders'));
     } // end mehtod
 
@@ -61,11 +61,47 @@ class OrderController extends Controller
     } // end mehtod
 
     public function PendingToConfirm($order_id){
-        Order::findOrFail($order_id)->update(['status' => 'confirm']);
+        Order::findOrFail($order_id)->update(['status' => 'confirmed']);
         $notification = array(
-            'message' => 'Order Confirm Successfully',
+            'message' => 'Order Confirmed Successfully',
             'alert-type' => 'success'
         );
         return redirect()->route('pending-orders')->with($notification);
+    } // end method
+
+    public function ConfirmToProcessing($order_id){
+        Order::findOrFail($order_id)->update(['status' => 'processing']);
+        $notification = array(
+            'message' => 'Order Processing Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('confirmed-orders')->with($notification);
+    } // end method
+
+    public function ProcessingToPicked($order_id){
+        Order::findOrFail($order_id)->update(['status' => 'picked']);
+        $notification = array(
+            'message' => 'Order Picked Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('processing-orders')->with($notification);
+    } // end method
+
+    public function PickedToShipped($order_id){
+        Order::findOrFail($order_id)->update(['status' => 'shipped']);
+        $notification = array(
+            'message' => 'Order Shipped Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('picked-orders')->with($notification);
+    } // end method
+
+    public function ShippedToDelivered($order_id){
+        Order::findOrFail($order_id)->update(['status' => 'delivered']);
+        $notification = array(
+            'message' => 'Order Delivered Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('shipped-orders')->with($notification);
     } // end method
 }
