7eba5e47c1ef1c4a310eca329dab67887e75ca9b
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-09-11 16:30:53 +0000

    user role ACL edit and update

--- yeyam_shop/routes/web.php
+++ yeyam_shop/routes/web.php
@@ -420,4 +420,8 @@ Route::prefix('adminuserrole')->group(function(){
     Route::get('/add', [AdminUserController::class, 'AddAdminRole'])->name('add.admin');
 
     Route::post('/store', [AdminUserController::class, 'StoreAdminRole'])->name('admin.user.store');
+
+    Route::get('/edit/{id}', [AdminUserController::class, 'EditAdminRole'])->name('edit.admin.user');
+
+    Route::post('/update', [AdminUserController::class, 'UpdateAdminRole'])->name('admin.user.update');
 });
