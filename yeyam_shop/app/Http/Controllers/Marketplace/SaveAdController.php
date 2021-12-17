8eb4695c37af81bfd860004436b64bbe4d45314d
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-12-17 12:21:21 +0000

    adding component save ad

---
+++ yeyam_shop/app/Http/Controllers/Marketplace/SaveAdController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Marketplace;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SaveAdController extends Controller
+{
+    //
+}
